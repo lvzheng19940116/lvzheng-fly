dcf891ca260ea35a98923366385dd9cde1c36d66
lvzheng <lvzheng@samsundot.com>
2018-07-04 11:20:19 +0800

    addinit

---
+++ src/main/java/flylvzheng/SpringcloudLvzhengApplication.java
@@ -0,0 +1,12 @@
+package flylvzheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudLvzhengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudLvzhengApplication.class, args);
+	}
+}
