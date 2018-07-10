5f656cf459593111f28f268b8ba8016eb0f9acda
lvzheng <lvzheng@samsundot.com>
2018-07-10 17:35:19 +0800

    上传下载

---
+++ src/main/java/flylvzheng/controller/UploadDownController.java
@@ -0,0 +1,151 @@
+package flylvzheng.controller;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+/**
+*以动手实践为荣,以只看不练为耻.
+*以打印日志为荣,以出错不报为耻.
+*以局部变量为荣,以全局变量为耻.
+*以单元测试为荣,以手工测试为耻.
+*以代码重用为荣,以复制粘贴为耻.
+*以多态应用为荣,以分支判断为耻.
+*以定义常量为荣,以魔法数字为耻.
+*以总结思考为荣,以不求甚解为耻.
+*
+* @author LvZheng
+* 创建时间：2018年7月10日 上午11:43:45
+*/
+@RestController
+public class UploadDownController {
+	
+	private static final Logger log = LoggerFactory.getLogger(UploadDownController.class);
+
+    @RequestMapping(value = "/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                return "文件为空";
+            }
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            log.info("上传的文件名为：" + fileName);
+            // 获取文件的后缀名
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            log.info("文件的后缀名为：" + suffixName);
+            // 设置文件存储路径
+            String filePath = "/Users/lvzheng/Downloads/";
+            String path = filePath + fileName;
+            File dest = new File(path);
+            // 检测是否存在目录
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();// 新建文件夹
+            }
+            file.transferTo(dest);// 文件写入
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+
+    @PostMapping("/batch")
+    public String handleFileUpload(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        MultipartFile file = null;
+        BufferedOutputStream stream = null;
+        for (int i = 0; i < files.size(); ++i) {
+            file = files.get(i);
+            String filePath = "/Users/lvzheng/Downloads/";
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    stream = new BufferedOutputStream(new FileOutputStream(
+                            new File(filePath + file.getOriginalFilename())));//设置文件路径及名字
+                    stream.write(bytes);// 写入
+                    stream.close();
+                } catch (Exception e) {
+                    stream = null;
+                    return "第 " + i + " 个文件上传失败 ==> "
+                            + e.getMessage();
+                }
+            } else {
+                return "第 " + i
+                        + " 个文件上传失败因为文件为空";
+            }
+        }
+        return "上传成功";
+    }
+
+    @GetMapping("/download")
+    public String downloadFile(HttpServletRequest request, HttpServletResponse response) {
+        String fileName = "bankacceptance.sql";// 文件名
+        if (fileName != null) {
+            //设置文件路径
+            File file = new File("/Users/lvzheng/Downloads/bankacceptance.sql");
+            //File file = new File(realPath , fileName);
+            if (file.exists()) {//判断文件父目录是否存在
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return "下载成功";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return "下载失败";
+    }
+
+
+	
+	
+	
+}
