ac74d8dac68f0f0c76f7c9453a571c7e06e77aab
lvzheng <lvzheng@samsundot.com>
2018-08-15 10:09:25 +0800

    add

--- src/main/java/flylvzheng/bean/Emp.java
+++ src/main/java/flylvzheng/bean/Emp.java
@@ -2,24 +2,17 @@ package flylvzheng.bean;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import flylvzheng.bean.world.User;
 import lombok.Data;
 
 /**
