a4f0cf81979941eab9cde2d9204a2fb1698a4420
lvzheng <lvzheng@samsundot.com>
2019-06-28 14:17:53 +0800

    jdbc

--- src/main/java/flylvzheng/bean/Emp.java
+++ src/main/java/flylvzheng/bean/Emp.java
@@ -1,15 +1,13 @@
 package flylvzheng.bean;
 
 import java.util.Date;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
+import javax.persistence.criteria.JoinType;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -23,28 +21,28 @@ import lombok.Data;
 @Data
 @Entity
 public class Emp {
-	/**
-	 * JPA提供的四种标准用法为TABLE,SEQUENCE,IDENTITY,AUTO. 
-	 * TABLE：使用一个特定的数据库表格来保存主键。 
-	 * SEQUENCE：根据底层数据库的序列来生成主键，条件是数据库支持序列。 
-	 * IDENTITY：主键由数据库自动生成（主要是自动增长型） 
-	 * AUTO：主键由程序控制。 
-	 */
+    /**
+     * JPA提供的四种标准用法为TABLE,SEQUENCE,IDENTITY,AUTO. 
+     * TABLE：使用一个特定的数据库表格来保存主键。 
+     * SEQUENCE：根据底层数据库的序列来生成主键，条件是数据库支持序列。 
+     * IDENTITY：主键由数据库自动生成（主要是自动增长型） 
+     * AUTO：主键由程序控制。 
+     */
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int eid;
-	private String name;
-	private String sect;
-	private String effort;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int eid;
+    private String name;
+    private String sect;
+    private String effort;
     @CreationTimestamp
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(updatable = false)
-	private Date date1;
-	@Temporal(TemporalType.TIMESTAMP)
-	@UpdateTimestamp
-	private Date date2;
-	// private int gid;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    private Date date1;
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    private Date date2;
+    // private int gid;
 
 
 //	CascadeType.PERSIST：级联新增（又称级联保存）：对order对象保存时也对items里的对象也会保存。对应EntityManager的presist方法。
@@ -67,15 +65,33 @@ public class Emp {
 //	@JsonBackReference
 //	private User user;
 
+/**
+ *
+ *
+ * jpa 多对一 模糊查询
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "account", referencedColumnName = "accNbr", insertable = false, updatable = false)
+    private BaseAccounts baseAccounts;
 
-	public Emp(String name, String sect, String effort, Date date1, Date date2) {
-		this.name = name;
-		this.sect = sect;
-		this.effort = effort;
-		this.date1 = date1;
-		this.date2 = date2;
-	}
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<BizMoneyRecords> bizMoneyRecordsList;
 
-	public Emp() {
-	}
+    if(StringUtils.isNotBlank(bankmoneyrecordsForm.getSoName()))
+    {
+        Join<BizMoneyRecords, BaseAccounts> join = root.join("baseAccounts", JoinType.LEFT);
+        list.add(criteriaBuilder.equal(join.<String>get("soName"), bankmoneyrecordsForm.getSoName()));
+    }
+*/
+
+    public Emp(String name, String sect, String effort, Date date1, Date date2) {
+        this.name = name;
+        this.sect = sect;
+        this.effort = effort;
+        this.date1 = date1;
+        this.date2 = date2;
+    }
+
+    public Emp() {
+    }
 }
