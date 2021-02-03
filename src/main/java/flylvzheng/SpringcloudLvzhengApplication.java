d894e97ec11cecabcb5833abe31094a9f115857c
lvzheng <18301161405@163.com>
2021-02-03 17:20:59 +0800

    'swagger'

--- src/main/java/flylvzheng/SpringcloudLvzhengApplication.java
+++ src/main/java/flylvzheng/SpringcloudLvzhengApplication.java
@@ -1,17 +1,14 @@
 package flylvzheng;
 
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 //@EnableEurekaClient
 //@EnableFeignClients
-@EnableSwagger2
-@EnableSwaggerBootstrapUI//doc.html    /swagger-ui.html
+//@EnableSwaggerBootstrapUI//doc.html    /swagger-ui.html
 @EnableAsync
 public class SpringcloudLvzhengApplication {
 
